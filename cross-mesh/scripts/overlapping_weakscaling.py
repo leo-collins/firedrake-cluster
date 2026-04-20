06c19b963f0993b521db0f5263fd1fc928ddac64
Leo Collins <collins.leo@proton.me>
2026-04-20 21:49:17 +0100

    fix csv

--- cross-mesh/scripts/overlapping_weakscaling.py
+++ cross-mesh/scripts/overlapping_weakscaling.py
@@ -60,9 +60,9 @@ if COMM_WORLD.rank == 0:
                 f,
                 fieldnames=[
                     "nprocs",
+                    "degree",
                     "dofs_per_core",
                     "mesh_gen_time_s",
-                    "assembly_time_s",
                     "run0",
                     "run1",
                     "run2",
@@ -74,6 +74,7 @@ if COMM_WORLD.rank == 0:
             w.writerow(
                 {
                     "nprocs": n_cores,
+                    "degree": degree,
                     "dofs_per_core": average_dofs_per_core,
                     "mesh_gen_time_s": mesh_gen_time_s,
                     "run0": run_times_s[0],
